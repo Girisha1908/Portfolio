a636d33beb2f95bca39570ae9edc58040830bec0
Girisha Anamala <anamalagirisha@gmail.com>
2026-04-23 00:18:56 +0530

    Refined UI: Optimized Skills hierarchy, normalized card layouts, and updated networking links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,17 +23,17 @@ export default function Navbar() {
             <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-full" />
           </a>
           <a
-            href="#leadership"
-            className="text-white/60 hover:text-white transition-colors relative group"
-          >
-            Volunteering
-            <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-full" />
-          </a>
-          <a
             href="#projects"
             className="text-white/60 hover:text-white transition-colors relative group"
           >
             Projects
+            <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-full" />
+          </a>
+          <a
+            href="#leadership"
+            className="text-white/60 hover:text-white transition-colors relative group"
+          >
+            Volunteering
             <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-300 group-hover:w-full" />
           </a>
           <a
